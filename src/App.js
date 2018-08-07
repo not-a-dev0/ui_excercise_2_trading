3681c6f61ab00c2b65ecff7c070e7388ed3a2444
Serge Mykhailov <sergei@mikhailov.com.ua>
2018-08-08 01:15:39 +0200

    create layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react';
+import Card from './Card';
 import './App.css';
 
 class App extends Component {
+  state = { prices: [] };
+
+  componentDidMount() {
+    const url = 'http://www.mocky.io/v2/5b68d5923300008a3a32ddb4';
+
+    fetch(url)
+      .then(response => response.json())
+      .then(data => this.setState({ prices: data }))
+      .catch(error => console.log(error));
+  }
+
+  renderCards() {
+    const { prices } = this.state;
+    return prices.map(price => <Card key={price.pair} price={price} />);
+  }
+
   render() {
-    return <div>Init</div>;
+    return <div className="wrapper">{this.renderCards()}</div>;
   }
 }
 
